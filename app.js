d6e095f9e11fdfb2775cbfc477f2761ee619423c
Alp Serin <alpsrn@gmail.com>
2023-10-02 17:55:17 +0300

    Set up the routes and controllers structure

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager app hello");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+// app.get("/api/v1/tasks")         - get all the tasks
+// app.post("/api/v1/tasks")        - create a new task
+// app.get("/api/v1/tasks/:id")     - get single task
+// app.patch("/api/v1/tasks/:id")   - update task
+// app.delete("/api/v1/tasks/:id")  - delete task
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
